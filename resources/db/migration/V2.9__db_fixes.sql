bbc929cae5fee50c46461f22f93eec8da7332a4e
sylvain <sylvainmasclet24@gmail.com>
2023-05-12 15:01:10 +0300

    hotfix and commenting intent for V2.9 of sql migration.

--- resources/db/migration/V2.9__db_fixes.sql
+++ resources/db/migration/V2.9__db_fixes.sql
@@ -1,15 +1,24 @@
+-- Sylvain 12/05/23 --
+
+-- made big mistakes when first making the logs table, here i revamped it so that we can have the most important infos --
+-- also made it so that we have a field that makes the distinction between services and tutorials, may need to find something better --
+
 drop table Logs;
 create table Logs(
 	id bigserial primary key,
 	log_timestamp text,
 	service_or_tuto text,
-	result_id int,
+	result_id bigint,
 	created_at bigint,
 	updated_at bigint
 );
 
+
+-- dropped duplicated columns and app_id columns to prepare for the language amelioration which will actually let us use the ids used in the google sheets --
+
 alter table problems
 	drop column app_category_id;
+	drop column app_item_id;
 
 alter table results
 	drop column app_item_id,
@@ -27,6 +36,9 @@ alter table services
 	alter column app_service_type_id TYPE bigint using app_service_type_id::bigint;
 
 
+-- remade the problem_services table to only keep the most useful infos --
+-- TO DO: find out how to use a concatenated key so that instead of giving another field we can just use problem_id + service_id as the Primary Key --
+
 drop table problem_services;
 create table problem_services (
     id bigserial primary key,
@@ -38,14 +50,21 @@ create table problem_services (
 	foreign key (service_id) references services(id)
 );
 
+
+-- implementing the service types with the downside of having to use an "app_id", explanation below with content type and skill levels
+
 create table service_types (
-	id bigint primary key,
+	id bigserial primary key,
 	type_name text,
 	lang text,
+	app_id bigint,
 	created_at bigint,
 	updated_at bigint
 );
 
+
+
+-- i made a mistake and copied how the Payload works --
 
 alter table skill_level
     rename column min_skill_en to label;
@@ -59,3 +78,13 @@ alter table content_type
 
 alter table content_type
     rename column content_type_fi to lang;
+
+
+-- adding as its own column the real id (the one used in the google sheets) so that we can link tables with it while also having the lang field useable --
+
+
+alter table content_type
+    add column app_id bigint;
+
+alter table skill_level
+    add column app_id bigint;
